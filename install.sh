79377886d4e7b0e0bc32b4b1e0417e9a632f453e
Charl van Niekerk <charlvn@charlvn.com>
2011-11-28 13:43:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
 # Install packages
-apt-get install -y xorg openbox alsa aufs-tools tint2 chromium-browser flashplugin-nonfree tor tor-geoipdb xfce4-terminal vim git vlc nmap zenmap xchat miredo vidalia gksu bzip2 bcrypt
+apt-get install -y xorg openbox alsa aufs-tools tint2 chromium-browser flashplugin-nonfree tor tor-geoipdb xfce4-terminal vlc xchat miredo vidalia gksu bzip2 bcrypt
 
 # Install Thunderbird
 wget -O /tmp/thunderbird.tar.bz2 http://www.mirrorservice.org/sites/releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/linux-i686/en-US/thunderbird-8.0.tar.bz2
@@ -29,12 +29,12 @@ amixer set Mic 100 unmute
 # Configure Openbox
 sed -i '/^exit 0/isu user -c startx' /etc/rc.local
 sed -i '/<number>/c<number>1</number>' /etc/xdg/openbox/rc.xml
-wget --no-check-certificate -O /etc/xdg/openbox/menu.xml https://raw.github.com/charlvanniekerk/hypervenster/master/menu.xml
+wget --no-check-certificate -O /etc/xdg/openbox/menu.xml https://raw.github.com/ushahidi/dpr/master/menu.xml
 
 # Configure tint2
 echo 'tint2 &' >> /etc/xdg/openbox/autostart.sh
 mkdir -p /home/user/.config/tint2
-wget --no-check-certificate -O /home/user/.config/tint2/tint2rc https://raw.github.com/charlvanniekerk/hypervenster/master/tint2rc
+wget --no-check-certificate -O /home/user/.config/tint2/tint2rc https://raw.github.com/ushahidi/dpr/master/tint2rc
 chown -R user:user /home/user/.config
 
 # Configure Vidalia
@@ -51,7 +51,7 @@ update-grub
 
 # Configure aufs as root
 echo aufs >> /etc/initramfs-tools/modules
-wget --no-check-certificate -O /etc/initramfs-tools/scripts/init-bottom/rootaufs https://raw.github.com/charlvanniekerk/hypervenster/master/rootaufs.sh
+wget --no-check-certificate -O /etc/initramfs-tools/scripts/init-bottom/rootaufs https://raw.github.com/ushahidi/dpr/master/rootaufs.sh
 chmod +x /etc/initramfs-tools/scripts/init-bottom/rootaufs
 update-initramfs -u
 
